9e2a447df89718e18459a534a95752088dedfe92
Philippe <ducassephi@hotmail.fr>
2023-05-15 16:59:59 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/Movie-card";
-import { MovieView } from "../movie-view/Movie-view";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+import Row from 'react-bootstrap/Row';// rows can be divided into twelfths
+import Col from 'react-bootstrap/Col';
+import "../main-view/main-view.scss";
+
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
