a33e9d5521f8047beb1c9018d7bec3110a77c19d
Philippe <ducassephi@hotmail.fr>
2023-10-07 16:40:39 +0200

    updated parcel dev dependencies

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -17,7 +17,7 @@ export const LoginView = ({ onLoggedIn }) => {
         Password: password
       };
   
-      fetch("https://fletnix-b399cde14eec.herokuapp.com/users", {
+      fetch("https://fletnix-b399cde14eec.herokuapp.com/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
